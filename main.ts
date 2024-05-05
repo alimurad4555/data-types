5863a43d7434e717cdcf718a241d19edd3f08014
alimurad4555 <alimurad4555gmail.com>
2024-05-05 02:22:39 -0700

    typescript data types

---
+++ main.ts
@@ -0,0 +1,9 @@
+let variable:string = "hello ali murad" 
+// variable : 10      type 'number' is not assignable to type 'string'
+let number:number = 10.10 
+// number : "hello"   type 'string' is not assignable to type 'number'
+
+let israining:boolean =true 
+// israining : 10     type 'number' is not assignable to type 'boolean'
+let anydata:any = 'hello'
+anydata = 10
